14aa7216ab6369fde2d4f9fab57a9c4e3880e418
Svarium <omuoz@encope.com.ar>
2023-02-01 18:45:00 -0300

    inicializo el proyecto con modelo MVC

---
+++ controllers/mainController.js
@@ -0,0 +1,29 @@
+/* const unidades = require('../data/unidades.json') */
+
+module.exports = {
+    home : (req,res) =>{
+        return res.render('index',{
+            title: 'Encope'
+        });
+    },
+    inicio : (req,res) =>{
+        return res.render('inicio', {
+            title: 'Encope'
+        })
+    },
+    nosotros : (req,res)=>{
+        return res.render('nosotros',{
+            title:'Nosotros'
+        })
+    },
+    licitaciones : (req,res)=>{
+        return res.render('licitaciones', {
+            title: 'Licitaciones'
+        })
+    },
+    interno : (req,res)=>{
+        return res.render('interno',{
+            title : 'Uso interno'
+        })
+    }
+}
