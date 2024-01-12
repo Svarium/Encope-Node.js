34609dbe509968b58233baa83245fbbbfef3da0c
John Doe <svariumfoo@gmail.com>
2024-01-12 18:14:09 -0300

    controlador y boton para reporte en excel de tabla productos terminados

--- src/controllers/stockController.js
+++ src/controllers/stockController.js
@@ -210,35 +210,6 @@ module.exports = {
         }).catch(errors => console.log(errors))
     },
 
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
     estadisticas: (req,res) => {
 
         const productos = db.Producto.findAll({
@@ -258,28 +229,18 @@ module.exports = {
             })
         })
       
-    },
+    }, 
 
- 
-
- 
-
-
- 
-
-
- 
-
-    descargarTablaStock : async (req,res) => {      
+    productsTableExcel : async (req,res) => {      
 
         try {
-            const datosStock = await getGeneralStock(); // Traigo mi consulta de stock
+            const tablaProductos = await db.Producto.findAll(); // Traigo mi consulta de stock
     
             const workbook = new ExcelJS.Workbook(); // Función constructora del Excel
             const worksheet = workbook.addWorksheet('Sheet 1'); // Crea una hoja de Excel (CREO)
     
             // Agregar títulos de columnas
-            const titleRow = worksheet.addRow(["Nombre Destino", "Nombre Producto", "Cantidad", "Actualizado el"]);
+            const titleRow = worksheet.addRow(["Nombre Producto", "Detalle", "Unidad de Medida", "Creado"]);
     
             // Aplicar formato al título
             titleRow.eachCell((cell) => {
@@ -299,8 +260,8 @@ module.exports = {
                 };
             });
     
-            datosStock.forEach(stock => {
-                const row = worksheet.addRow([stock.destino.nombreDestino, stock.producto.nombre, stock.cantidad, stock.updatedAt]);
+            tablaProductos.forEach(producto => {
+                const row = worksheet.addRow([producto.nombre, producto.detalle, producto.unidadDeMedida, producto.createdAt]);
                 
                 // Aplicar bordes a las celdas de la fila de datos
                 row.eachCell((cell) => {
@@ -330,124 +291,5 @@ module.exports = {
 
     },
 
-    descargarTablaRetirosStock : async (req,res) => {
-        try {
-            const datosStock = await db.detalleRetiro.findAll({
-                include:["destino", "producto"],
-                order:[["updatedAt", "DESC"]]
-            }) ; // Traigo mi consulta de stock
-
-         /*    return res.send(datosStock) */
-    
-            const workbook = new ExcelJS.Workbook(); // Función constructora del Excel
-            const worksheet = workbook.addWorksheet('Sheet 1'); // Crea una hoja de Excel (CREO)
-    
-            // Agregar títulos de columnas
-            const titleRow = worksheet.addRow(["Nombre Destino", "Nombre Producto", "Cantidad Retirada", "Actualizado el"]);
-    
-            // Aplicar formato al título
-            titleRow.eachCell((cell) => {
-                cell.font = { bold: true }; // Establece el texto en negrita
-                cell.fill = {
-                    type: 'pattern',
-                    pattern: 'solid',
-                    fgColor: { argb: 'FFFF00' } // Cambia el color de fondo a amarillo (puedes cambiar 'FFFF00' por el código del color que prefieras)
-                };
-    
-                // Agregar bordes
-                cell.border = {
-                    top: { style: 'thin' },
-                    left: { style: 'thin' },
-                    bottom: { style: 'thin' },
-                    right: { style: 'thin' }
-                };
-            });
-    
-            datosStock.forEach(stock => {
-                const row = worksheet.addRow([stock.destino.nombreDestino, stock.producto.nombre, stock.cantidadRetirada, stock.updatedAt]);
-                
-                // Aplicar bordes a las celdas de la fila de datos
-                row.eachCell((cell) => {
-                    cell.border = {
-                        top: { style: 'thin' },
-                        left: { style: 'thin' },
-                        bottom: { style: 'thin' },
-                        right: { style: 'thin' }
-                    };
-                });
-            });
-    
-            const fecha = new Date(Date.now());
-    
-            // Define el nombre del archivo Excel
-            res.setHeader('Content-Disposition', `attachment; filename="${fecha.toISOString().substring(0, 10)}-RetirosStock.xlsx"`); // agregar al nombre la fecha con New Date()
-            res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    
-            // Envia el archivo Excel como respuesta al cliente
-            await workbook.xlsx.write(res);
-    
-            // Finaliza la respuesta
-            res.end();
-        } catch (error) {
-            console.log(error);
-        }
-    },
-
-    descargarTablaStockCPFII: async (req,res) => {
-        try {
-            const datosStock = await getAllStocks(); // Traigo mi consulta de stock
-    
-            const workbook = new ExcelJS.Workbook(); // Función constructora del Excel
-            const worksheet = workbook.addWorksheet('Sheet 1'); // Crea una hoja de Excel (CREO)
-    
-            // Agregar títulos de columnas
-            const titleRow = worksheet.addRow(["Nombre Destino", "Nombre Producto", "Cantidad", "Actualizado el"]);
-    
-            // Aplicar formato al título
-            titleRow.eachCell((cell) => {
-                cell.font = { bold: true }; // Establece el texto en negrita
-                cell.fill = {
-                    type: 'pattern',
-                    pattern: 'solid',
-                    fgColor: { argb: 'FFFF00' } // Cambia el color de fondo a amarillo (puedes cambiar 'FFFF00' por el código del color que prefieras)
-                };
-    
-                // Agregar bordes
-                cell.border = {
-                    top: { style: 'thin' },
-                    left: { style: 'thin' },
-                    bottom: { style: 'thin' },
-                    right: { style: 'thin' }
-                };
-            });
-    
-            datosStock.forEach(stock => {
-                const row = worksheet.addRow([stock.usuario.destino.nombreDestino, stock.producto.nombre, stock.cantidad, stock.updatedAt]);
-                
-                // Aplicar bordes a las celdas de la fila de datos
-                row.eachCell((cell) => {
-                    cell.border = {
-                        top: { style: 'thin' },
-                        left: { style: 'thin' },
-                        bottom: { style: 'thin' },
-                        right: { style: 'thin' }
-                    };
-                });
-            });
-    
-            const fecha = new Date(Date.now());
-    
-            // Define el nombre del archivo Excel
-            res.setHeader('Content-Disposition', `attachment; filename="${fecha.toISOString().substring(0, 10)}-stockCPFII.xlsx"`); // agregar al nombre la fecha con New Date()
-            res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    
-            // Envia el archivo Excel como respuesta al cliente
-            await workbook.xlsx.write(res);
-    
-            // Finaliza la respuesta
-            res.end();
-        } catch (error) {
-            console.log(error);
-        }
-    }
+  
 }
--- src/routes/stock.js
+++ src/routes/stock.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { list, estadisticas, descargarTablaStock, descargarTablaRetirosStock, descargarTablaStockCPFII, newProduct, storageProduct, listProducts, editProduct, updateProduct, deleteProduct, searchProduct } = require('../controllers/stockController');
+const { list, estadisticas, descargarTablaStock, descargarTablaRetirosStock, descargarTablaStockCPFII, newProduct, storageProduct, listProducts, editProduct, updateProduct, deleteProduct, searchProduct, productsTableExcel } = require('../controllers/stockController');
 
 const checkUserEditorIntranet = require('../middlewares/checkUserEditorIntranet');
 const { uploadProductosFiles } = require('../middlewares/subirProductos');
@@ -23,6 +23,7 @@ router.get('/editProduct/:id', editProduct)
 router.put('/editProduct/:id',uploadProductosFiles.single('producto'),addProductValidator, updateProduct)
 router.delete('/delete/:id', deleteProduct)
 router.get('/searchProduct', searchProduct)
+router.get('/ProductsTable', productsTableExcel)
 
 
 module.exports = router;
