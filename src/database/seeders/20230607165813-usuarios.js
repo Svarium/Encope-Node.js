b5f2634a918f2c1eaf253764551bb7937afb68ae
jnatmed <49349742+jnatmed@users.noreply.github.com>
2024-06-24 14:19:02 -0300

    cambios en fondo y links al nuevo sistema
    1) Se cambian links para que apunten al dashboard en:
    - adminLicitaciones.ejs
    - editarLic.ejs
    - estadistica.ejs
    2) Se cambio background "'/images/img\/fondo.png'" en vistas:
    - agregarLic.ejs
    - searchLicitacion.ejs
    - searchUser.ejs
    - dashboard.ejs
    - editarLic.ejs
    - resultado.ejs
    3) Se creo menu responsive en intranet.ejs
    4) Se cambio nombre en unidades.json: donde dice Sede Central por Ente de Cooperacion Tenica y Financiera
    5) se oculto boton tutorialModuloCunitas, ya que el mismo esta en desuso. Vista perfil.ejs
    6) se cambia link para q apunte a perfil donde dice aparece el nombre del perfil. Vista estadistica.ejs

--- src/database/seeders/20230607165813-usuarios.js
+++ src/database/seeders/20230607165813-usuarios.js
@@ -6,20 +6,22 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     
-      await queryInterface.bulkInsert('Usuarios', [{
-        name: 'Ezequiel',
-        surname : 'Muñoz',
-        email : 'svariumfoo@gmail.com',      
-        icon : 'https://lh3.googleusercontent.com/a/ACg8ocKAyV2iuOjK40tmmIlfcXKT9gOj4b1xdDkTG9Xz-NQXce4=s96-c',
-        socialId: '117303958612759707634',
-        socialProvider:'google',
-        destinoId:1,
-        credencial: 42942,
-        rolId: 1, 
-        destinoId:1,     
-        createdAt: new Date,  
-        updatedAt: new Date
-      }], {});
+      await queryInterface.bulkInsert('Usuarios', [
+        {
+          name: 'Ezequiel',
+          surname : 'Muñoz',
+          email : 'svariumfoo@gmail.com',      
+          icon : 'https://lh3.googleusercontent.com/a/ACg8ocKAyV2iuOjK40tmmIlfcXKT9gOj4b1xdDkTG9Xz-NQXce4=s96-c',
+          socialId: '117303958612759707634',
+          socialProvider:'google',
+          destinoId:1,
+          credencial: 42942,
+          rolId: 1, 
+          destinoId:1,     
+          createdAt: new Date,  
+          updatedAt: new Date
+        }
+    ], {});
     
   },
 
