8acd093694a6c846c73f39d1903f52bf5cbd366b
Svarium <svariumfoo@gmail.com>
2024-07-22 16:16:33 -0300

    arreglo error en enrutador en la ruta de edicion de producto

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -229,7 +229,7 @@ module.exports = {
             where:{id:id}
         })
         .then(() => {
-            return res.redirect('/stock/products')
+            return res.redirect('/')
         })
         .catch(error => console.log(error));
     } else {
